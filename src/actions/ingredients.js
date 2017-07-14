b98a3a5a3995a7ac63df10c5371a6377664c68a6
Michael Martinez <martinez1212@gmail.com>
2017-07-14 13:34:11 -0700

    reordered function syntax

--- src/actions/ingredients.js
+++ src/actions/ingredients.js
@@ -1,8 +1,10 @@
 import { API_URL } from '../config/api';
 
-export const getIngredients = () => {
-    return { type: 'GET_INGREDIENTS', payload: fetchGetIngredients() };
-};
+export const fetchGetIngredients = () => fetch(`${API_URL}/api/v1/ingredients`)
+.then(res => res.json());
 
-export const fetchGetIngredients = () => fetch(`${API_URL}/api/v1/ingredients`).then(res => res.json())
-.then(res => console.log('this is the res', res));
+export const getAllIngredients = () => (
+  { type: 'GET_ALL_INGREDIENTS', payload: fetchGetIngredients() }
+);
+
+// export const Get
