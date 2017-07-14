a1e68eea4b9ffbeb88a94649b99b48df7ac3be73
Michael Martinez <martinez1212@gmail.com>
2017-07-14 13:32:48 -0700

    added columns to render all ingredients

--- src/components/ingredients/ingredients.jsx
+++ src/components/ingredients/ingredients.jsx
@@ -1,20 +1,89 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { getIngredients } from '../../actions/ingredients';
+import { Grid, Image } from 'semantic-ui-react';
+import { getAllIngredients } from '../../actions/ingredients';
+import rootReducer from '../../reducers';
 
 export class IngredientView extends Component {
 
-  componentDidMount() {
-    this.props.getIngredients();
+  componentWillMount() {
+    return this.props.getAllIngredients();
+  }
+
+  shuffleArray = (array) => {
+    let shuffledArray = array.slice();
+    let len = shuffledArray.length - 1;
+    let result = [];
+    let randomInt;
+    let temp;
+
+    while (len > 0) {
+      randomInt = Math.floor(Math.random() * len);
+      temp = shuffledArray[len];
+      shuffledArray[len] = shuffledArray[randomInt];
+      shuffledArray[randomInt] = temp;
+      len--;
+    }
+    return shuffledArray
+  }
+
+  ingredientSplit = (arr, num) => {
+    let updatedArr = this.shuffleArray(arr);
+    let rest = updatedArr.length % num;
+    let restUsed = rest;
+    let partLength = Math.floor(updatedArr.length / num);
+    let result = [];
+    for (var i = 0; i < updatedArr.length; i += partLength) {
+      let end = partLength + i;
+      let add = false;
+      if (rest !== 0 && restUsed) {
+        end++;
+        restUsed--;
+        add = true;
+      }
+      result.push(updatedArr.slice(i, end));
+      if (add) {
+        i++;
+      }
+    }
+    return result;
   }
 
   render() {
+    const { ingredients } = this.props;
+    // console.log('this is ingredient', this.props);
+    let usefulArray = this.ingredientSplit(ingredients.allIngredients, 5);
     return (
-      <div>
-        <h2>Hi</h2>
-      </div>
+      <Grid doubling columns={5}>
+    <Grid.Column>
+      {ingredients.allIngredients.length > 0 && usefulArray[0].map(item =>
+        <div key={item.id}>{item.name}</div>
+      )}
+    </Grid.Column>
+    <Grid.Column>
+      {ingredients.allIngredients.length > 1 && usefulArray[1].map(item =>
+        <div key={item.id}>{item.name}</div>
+      )}
+    </Grid.Column>
+    <Grid.Column>
+      {ingredients.allIngredients.length > 2 && usefulArray[2].map(item =>
+        <div key={item.id}>{item.name}</div>
+      )}
+    </Grid.Column>
+    <Grid.Column>
+      {ingredients.allIngredients.length > 3 && usefulArray[3].map(item =>
+        <div key={item.id}>{item.name}</div>
+      )}
+    </Grid.Column>
+    <Grid.Column>
+      {ingredients.allIngredients.length > 4 && usefulArray[4].map(item =>
+        <div key={item.id}>{item.name}</div>
+      )}
+    </Grid.Column>
+  </Grid>
+
     )
   }
 }
 
-export default connect(null, { getIngredients })(IngredientView);
+export default connect(rootReducer, { getAllIngredients })(IngredientView);
